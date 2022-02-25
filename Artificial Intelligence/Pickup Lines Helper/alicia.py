209a04268559c8ba95b68155a5101785f320313c
Atharv Salian <atharv44@gmail.com>
2022-02-25 16:38:48 +0530

    creating intents

--- Artificial Intelligence/Pickup Lines Helper/alicia.py
+++ Artificial Intelligence/Pickup Lines Helper/alicia.py
@@ -13,3 +13,14 @@ import json
 with open("/Users/atharvsalian/Desktop/Github/AI-ML/Artificial Intelligence/Pickup Lines Helper/pickuplines.json") as file:
     data = json.load(file)
 
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent['patterns']:
+        word = nltk.word_tokenize(pattern)
+        words.extend(word)
+        docs_x.append(word)
+        docs_y.append(intent['tag'])
